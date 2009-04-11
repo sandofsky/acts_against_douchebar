50928203c730da51941019f03b7b6ba9a91a4484
Ben Sandofsky <sandofsky@gmail.com>
2009-04-10 21:49:54 -0700

    Add a frame-killer option.

--- init.rb
+++ init.rb
@@ -11,9 +11,19 @@ class ActionController::Base
     before_filter :acts_against_douchbar_filter
   end
   private
+  
+  # Thank wikipedia: http://en.wikipedia.org/wiki/Framekiller
+  @@douchbar_breakout_text = <<-END
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+  <head><script type="text/javascript">top.location.href = self.location.href</script></head>
+  <body>We are now redirecting you to the full quality site.</body>
+</html>
+  END
+  
   def acts_against_douchbar_filter
     if request.referer =~ %r{http://digg.com/\w{1,8}/*(\?.*)?$}
-      render :text => @@douchbar_filter_message
+      render :text => (@@douchbar_filter_message == :frame_killer ? @@douchbar_breakout_text : @@douchbar_filter_message)
       return false
     else
       return true
